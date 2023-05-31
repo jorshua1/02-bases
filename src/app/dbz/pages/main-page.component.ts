725400738b0e45ae1a946bc0e0b8136de1231354
Jorshua <jorshua321@gmail.com>
2023-05-31 16:06:04 -0500

    ultimos cambios antes de despliegue

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -20,7 +20,7 @@ export class DbzComponent {
     this.dbzService.deleteCharacterById(id);
   }
 
-  onNewCharacter(character: Character): void {
+  onNewCharacter(character: Character) {
     this.dbzService.addCharacter(character);
   }
 }
